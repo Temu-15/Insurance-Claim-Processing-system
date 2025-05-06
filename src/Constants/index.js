3817ae763f4ddf4e073daea9ac80e726617fc162
Temu-15 <abuadanu89@gmail.com>
2025-05-06 16:36:33 +0300

    updated

---
+++ src/Constants/index.js
@@ -0,0 +1,24 @@
+const navbarmenu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "How it works",
+    link: "/howitworks",
+  },
+  {
+    id: 3,
+    title: "Features",
+    link: "/features",
+  },
+  {
+    id: 4,
+    title: "About us",
+    link: "/about",
+  },
+];
+
+export { navbarmenu };
